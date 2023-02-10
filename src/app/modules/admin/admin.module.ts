4c1fae391f90e2db2262d1545411fd886cd1ac3d
gentlock <kamil.lsx@gmail.com>
2023-02-10 23:37:48 +0100

    ze starego

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AdminEntryPointComponent } from './components/admin-entry-point/admin-entry-point.component';
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    AdminEntryPointComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/admin/components/admin-entry-point/admin-entry-point.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'studio-admin-entry-point',
+  templateUrl: './admin-entry-point.component.html',
+  styleUrls: ['./admin-entry-point.component.scss']
+})
+export class AdminEntryPointComponent {
+
+}
