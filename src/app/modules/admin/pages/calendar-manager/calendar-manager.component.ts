ba174362de338316667387a5be63f8769e4774ee
gentlock <adblue@eyed@gmail.com>
2023-02-10 21:35:36 +0100

    x

---
+++ src/app/modules/admin/pages/calendar-manager/calendar-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'studio-calendar-manager',
+  templateUrl: './calendar-manager.component.html',
+  styleUrls: ['./calendar-manager.component.scss']
+})
+export class CalendarManagerComponent {
+
+}
