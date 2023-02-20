6e6e4953ab96dd3556d057a42bd18ff9ff4624ab
gentlock <kamil.lsx@gmail.com>
2023-02-20 16:39:54 +0100

    z bowego

--- src/app/modules/admin/pages/albums-mgr/albums-mgr.component.ts
+++ src/app/modules/admin/pages/albums-mgr/albums-mgr.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {myPasswordGenerator, clearFormField} from "../../../../core/helpers/utils";
+import { AlbumsService } from "../../../../core/services/data/albums.service";
+import {Subject} from "rxjs";
+import {IAlbumsFeed} from "../../../../core/abstracts";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'studio-albums-mgr',
@@ -10,13 +14,16 @@ import {myPasswordGenerator, clearFormField} from "../../../../core/helpers/util
 export class AlbumsMgrComponent {
   myFormModel: FormGroup;
   clearField = clearFormField;
+  serviceName = 'albumsService';
+  eventsSubject: Subject<void> = new Subject<void>();
+
 
   constructor(
     private _fb: FormBuilder,
+    private albumsService: AlbumsService
   ) {
       this.myFormModel = _fb.group({
         'id':[''],
-        'action':[''],
         'isActive':[''],
         'name': ['', Validators.required],
         'clientName': ['', Validators.required],
@@ -38,26 +45,82 @@ export class AlbumsMgrComponent {
 
   ngOnSubmit = (e: Event) => {
     e.preventDefault();
+    let id = this.myFormModel.get('id')?.value;
+
+    const data: IAlbumsFeed = {
+      'isActive'      : this.myFormModel.get('isActive')?.value || false,
+      'name'          : this.myFormModel.get('name')?.value,
+      'clientName'    : this.myFormModel.get('clientName')?.value,
+      'clientEmail'   : this.myFormModel.get('clientEmail')?.value,
+      'accessCode'    : this.myFormModel.get('accessCode')?.value,
+      'desc'          : this.myFormModel.get('desc')?.value,
+      'serviceId'     : this.myFormModel.get('serviceId')?.value,
+      'clientInfo'    : this.myFormModel.get('clientInfo')?.value,
+    }
+
+    if(this.myFormModel.valid) {
+      if( id != "" ) {
+        this.albumsService.update(id, data).subscribe(
+          {
+            next: (value)=>{
+              // console.log(value);
+              this.refreshTableSignal();
+            },
+            error: (err)=>{ console.log(err)}
+          }
+        );
+      } else  {
+        this.albumsService.addNew(data).subscribe(
+          {
+            next: (value)=>{
+              // console.log(value);
+              this.refreshTableSignal();
+            },
+            error: (err)=>{ console.log(err)}
+          }
+        );
+      }
+    }
+
+  }
+
+  delete = ( id: string ) => {
+    this.albumsService.delete(id).subscribe(
+      {
+        next: (value)=>{ console.log(value)},
+        error: (err)=>{ console.log(err)}
+      })
+  }
+
+  populate = ( id: string ) => {
+    this.albumsService.getById( id ).subscribe(
+      {
+        next: (data) => {
+          this.myFormModel.get('id')?.setValue(id);
+          this.myFormModel.get('isActive')?.setValue(<boolean>data.isActive);
+          this.myFormModel.get('name')?.setValue(data.name);
+          this.myFormModel.get('clientName')?.setValue(data.clientName);
+          this.myFormModel.get('clientEmail')?.setValue(data.clientEmail);
+          this.myFormModel.get('accessCode')?.setValue(data.accessCode);
+          this.myFormModel.get('desc')?.setValue(data.desc);
+          this.myFormModel.get('serviceId')?.setValue(data.serviceId);
+          this.myFormModel.get('clientInfo')?.setValue(data.clientInfo);
+        },
+        error: (err: HttpErrorResponse) => { console.log(err) }
+      }
+    )
+  }
+
+  resetForm(e: Event) {
+    this.myFormModel.reset();
+  }
+
+  refreshTableSignal() {
+    this.eventsSubject.next();
   }
 
   generatePassword(event: Event) {
     event.preventDefault();
     this.myFormModel.get('accessCode')?.setValue( myPasswordGenerator.generate(8) );
   }
-
-  ngOnLinkClick(e: Event, id: any, action: string) {
-    e.preventDefault();
-
-      if(id && action === 'del') {
-        if(prompt('wpisz: tak') === 'tak') {
-          // this.localeDeleteService(uniqId);
-          // this.resetAll();
-          // this.updateTable();
-        } else {
-          alert("niepoprawny numer id !!");
-        }
-      } else {
-        // this.localPopulate(uniqId);
-      }
-    }
 }
--- src/app/modules/admin/pages/myservices-mgr/myservices-mgr.component.ts
+++ src/app/modules/admin/pages/myservices-mgr/myservices-mgr.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import { MyservicesService } from "../../../../core/services/data/myservices.service";
 import {clearFormField} from "../../../../core/helpers/utils";
+import {Subject} from "rxjs";
+import {IMyserviceFeed} from "../../../../core/abstracts";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'studio-myservices-mgr',
@@ -10,9 +14,13 @@ import {clearFormField} from "../../../../core/helpers/utils";
 export class MyservicesMgrComponent {
   myFormModel: FormGroup;
   clearField = clearFormField;
+  serviceName = 'myservicesService';
+  eventsSubject: Subject<void> = new Subject<void>();
+
 
   constructor(
   private _fb: FormBuilder,
+  private myservicesService: MyservicesService
 
 ) {
   this.myFormModel = _fb.group({
@@ -20,12 +28,76 @@ export class MyservicesMgrComponent {
     'action':['action'],
     'isActive':[''],
     'name': ['', Validators.required],
+    'subtitle': ['', Validators.required],
     'desc': ['', Validators.required],
     'priceList': ['', Validators.required],
   });
 }
+  refreshTableSignal() {
+    this.eventsSubject.next();
+  }
+
+  delete = ( id: string ) => {
+    this.myservicesService.delete(id).subscribe(
+      {
+        next: (value)=>{ console.log(value)},
+        error: (err)=>{ console.log(err)}
+      })
+  }
+
+  populate = ( id: string ) => {
+    this.myservicesService.getById( id ).subscribe(
+      {
+        next: (data) => {
+          this.myFormModel.get('id')?.setValue(id);
+          this.myFormModel.get('isActive')?.setValue(<boolean>data.isActive);
+          this.myFormModel.get('name')?.setValue(data.name);
+          this.myFormModel.get('desc')?.setValue(data.desc);
+          this.myFormModel.get('subtitle')?.setValue(data.subtitle);
+        },
+        error: (err: HttpErrorResponse) => { console.log(err) }
+      }
+    )
+  }
+
+  resetForm(e: Event) {
+    this.myFormModel.reset();
+  }
 
   ngOnSubmit = (e: Event) => {
     e.preventDefault();
+    let id = this.myFormModel.get('id')?.value;
+
+    const data: IMyserviceFeed = {
+      'isActive'    : this.myFormModel.get('isActive')?.value || false,
+      'name'        : this.myFormModel.get('name')?.value,
+      'subtitle'    : this.myFormModel.get('subtitle')?.value,
+      'desc'        : this.myFormModel.get('desc')?.value,
+      'priceList'   : this.myFormModel.get('priceList')?.value
+    }
+
+    if(this.myFormModel.valid) {
+      if( id != "" ) {
+        this.myservicesService.update(id, data).subscribe(
+          {
+            next: (value)=>{
+              // console.log(value);
+              this.refreshTableSignal();
+            },
+            error: (err)=>{ console.log(err)}
+          }
+        );
+      } else  {
+        this.myservicesService.addNew(data).subscribe(
+          {
+            next: (value)=>{
+              // console.log(value);
+              this.refreshTableSignal();
+            },
+            error: (err)=>{ console.log(err)}
+          }
+        );
+      }
+    }
   }
 }
