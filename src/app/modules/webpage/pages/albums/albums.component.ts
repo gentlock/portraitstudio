cf48e5ba3c6562a49cf36b045bc6171e1fc65389
gentlock <kamil.lsx@gmail.com>
2023-02-25 19:05:35 +0100

    z nowego

--- src/app/modules/webpage/pages/albums/albums.component.ts
+++ src/app/modules/webpage/pages/albums/albums.component.ts
@@ -1,4 +1,8 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild, ViewChildren} from '@angular/core';
+import {Observable} from "rxjs";
+import {IAlbumsFeed, IMyserviceFeed} from "../../../../core/abstracts";
+import {MyservicesService} from "../../../../core/services/data/myservices.service";
+import {AlbumsService} from "../../../../core/services/data/albums.service";
 
 @Component({
   selector: 'studio-albums',
@@ -6,6 +10,38 @@ import {AfterViewInit, Component} from '@angular/core';
   styleUrls: ['./albums.component.scss']
 })
 export class AlbumsComponent implements AfterViewInit{
+  data$!: Observable<IMyserviceFeed[]>;
+  albums$: Observable<IAlbumsFeed[]>;
+  details$!: Observable<IAlbumsFeed>;
+  @ViewChild("smallNav") smallNav!: ElementRef;
+  showDetails = false;
+  id!: string;
+
+  constructor(
+    private myservicesService: MyservicesService,
+    private albumsService: AlbumsService,
+    // private changeDetector: ChangeDetectorRef
+  ) {
+    this.data$ = myservicesService.getAll();
+    this.albums$ = albumsService.getAll();
+  }
+
+  switchTab(e: Event) {
+    e.preventDefault();
+
+    (this.smallNav.nativeElement as HTMLUListElement).querySelectorAll('a').forEach(item=>{
+      item.classList.remove('underline');
+    });
+
+    (e.target as HTMLLIElement).classList.add('underline');
+  }
+
+  switchView(e: Event, id: string) {
+    this.showDetails = true;
+    this.id = id;
+
+    this.details$ = this.albumsService.getById(this.id);
+  }
   ngAfterViewInit() {
 
   }
